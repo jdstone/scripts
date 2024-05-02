a2080713a656a11ebb17e3ce744791992af6046f
J.D. Stone <jdstone1@gmail.com>
2024-05-02 13:31:10 -0700

    movie dirs script code cleanup & other changes

--- create_movie_dirs.sh
+++ create_movie_dirs.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-# create_movie_dirs.sh
+#title            :create_movie_dirs.sh
+#description      :This script moves movie files organized in a single directory to their own directory.
+#author           :J.D. Stone
+#date             :20240502
+#version          :1.0
 
-#find . -type f \( -iname "*.mkv" -o -iname "*.mp4" \) | sed 's/\.\///' | cut -d "(" -f 1 | sed 's/\.$//' | sed 's/\./ /g'
 
 i=0
 
-####movie_root=/mnt/storage/videos/movies
-movie_root=~/create_movie_directory_test
-directory_file_count_limit=2
+## SET YOUR MOVIE DIRECTORY
+movie_root=~/movie_directory
+## SET A LIMIT FOR HOW MANY FILES YOU WANT TO PROCESS PER RUN
+directory_file_count_limit=15
 
 cd "$movie_root" || exit 1
 
@@ -16,39 +20,50 @@ RED='\033[0;31m'
 # No Color
 NC='\033[0m'
 
-printf "${RED}!!!!  $(pwd) -- are you sure you want to continue with the current directory?  !!!!${NC}\n"
-printf "${RED}!!!!  $movie_root -- are you sure you want to continue with the current movie_root?  !!!!${NC}\n\n"
-
-read -p "Type Ctrl-c to exit or Enter to continue."
 echo
-read -p "Are you sure you pressed the correct key (Ctrl-c to exit or Enter to continue)?"
+printf "${RED}!!!!  $(pwd) -- Are you sure you want to continue with this movie directory?  !!!!${NC}\n"
+
+read -p "Press Ctrl-C to exit or Enter to continue"
+echo
+read -p "Are you sure you pressed the correct key (Ctrl-C to exit or Enter to continue)?"
 printf "\n\n"
 
-echo $(echo !==== Creating the new movie folder structure and copying movies to their new location ====! | tr [:lower:] [:upper:])
+echo $(echo !==== Creating the new movie directory structure and copying movies to their new location ====! | tr [:lower:] [:upper:])
 echo
 echo
 
 
 for file in *.mkv *.mp4 *.avi; do
-  movie_files=()
-  movie_path="$(pwd)/$folder"
-  new_movie_files=()
-  cleanup_msg=$(echo "^^^^  Files could not be verified! No cleanup will be performed! ^^^^" | tr [:lower:] [:upper:])
+  base_filename=$file
 
   if [ $i = $directory_file_count_limit ]; then break; fi
 
-  if ! ( [ "$file" = "*.mkv" ] || [ "$file" = "*.mp4" ] || [ "$file" = "*.avi" ] ); then
-    movie_name=$(echo $file | sed 's/\.\///' | cut -d "(" -f 1 | sed 's/\.$//' | sed 's/\./ /g')
-    file_name_wo_ext=${file%.*}
-    folder_name=$(echo $file_name_wo_ext | sed 's/\./ /g')
-    echo Movie Name: $movie_name
-    echo File w/o extension: $file_name_wo_ext
-    echo Folder name: $folder_name
-    echo -- Creating movie directory...
-    #mkdir "$movie_name"
-    echo -- Moving all associated movie files to movie directory...
-    #cp "$file_name_wo_ext"* "$movie_name"/
+  if ! ( [ "$base_filename" = "*.mkv" ] || [ "$base_filename" = "*.mp4" ] || [ "$base_filename" = "*.avi" ] ); then
+    movie_root_filename=$(echo $base_filename | sed 's/\.\///' | cut -d "(" -f 1 | sed 's/\.$//')
+    movie_titlename=$(echo $movie_root_filename | sed 's/\./ /g')
+    movie_titlename_upper=$(echo $movie_titlename | tr [:lower:] [:upper:])
+    file_name_wo_ext=${base_filename%.*}
+    echo ===========================================================================
+    echo "Processing $movie_titlename_upper"
+    echo ===========================================================================
+    echo
+
+    echo "    Creating movie directory..."
+    mkdir "$movie_titlename"
+    echo
+    echo "    Moving all associated movie files to movie directory..."
+    mv "$file_name_wo_ext"* "$movie_titlename"/
+    echo
     echo
     i=$((i+1))
   fi
 done
+
+echo
+
+if [ $i -gt 1 ] || [ $i -eq 0 ]; then
+  echo "$i movies were processed." && printf "\n\n"
+elif [ $i -eq 1 ]; then
+  echo "$i movie was processed." && printf "\n\n"
+fi
+
--- revert_movie_dirs.sh
+++ revert_movie_dirs.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# revert_movie_dirs.sh
-
-#find . -type f \( -iname "*.mkv" -o -iname "*.mp4" \) | sed 's/.\///' | cut -d "(" -f 1 | sed 's/\.$//' | sed 's/\./ /g'
+#title            :revert_movie_dirs.sh
+#description      :This script moves movie files organized in their own directory to a single directory.
+#                  Reverts process completed by create_movie_dirs.sh.
+#author           :J.D. Stone
+#date             :20240502
+#version          :1.0
 
 i=0
 j=0
 
-####movie_root=/mnt/storage/videos/movies
-movie_root=~/create_movie_directory_test
+## SET YOUR MOVIE DIRECTORY
+movie_root=~/movie_directory
 
 cd "$movie_root" || exit 1
 
@@ -16,59 +19,55 @@ RED='\033[0;31m'
 # No Color
 NC='\033[0m'
 
-printf "${RED}!!!!  $(pwd) -- are you sure you want to continue with the current directory?  !!!!${NC}\n"
-printf "${RED}!!!!  $movie_root -- are you sure you want to continue with the current movie_root?  !!!!${NC}\n\n"
-
-read -p "Press Ctrl-c to exit or Enter to continue."
 echo
-read -p "Are you sure you pressed the correct key (Ctrl-c to exit or Enter to continue)?"
+printf "${RED}!!!!  $(pwd) -- Are you sure you want to continue with this movie directory?  !!!!${NC}\n"
+
+read -p "Press Ctrl-C to exit or Enter to continue"
+echo
+read -p "Are you sure you pressed the correct key (Ctrl-C to exit or Enter to continue)?"
 printf "\n\n"
 
-echo $(echo !==== Reverting creation of movie directory changes ====! | tr [:lower:] [:upper:])
+echo $(echo !==== Reverting creation of individual movie directory changes ====! | tr [:lower:] [:upper:])
 echo
 echo
 
 
-find . -maxdepth 1 ! -path . -type d | sed 's/.\///' | while read -r folder; do
+find . -maxdepth 1 ! -path . -type d | sed 's/.\///' | while read -r directory; do
   movie_files=()
-  movie_path="$(pwd)/$folder"
+  movie_path="$(pwd)/$directory"
   new_movie_files=()
   cleanup_msg=$(echo "^^^^  Files could not be verified! No cleanup wiill be performed! ^^^^" | tr [:lower:] [:upper:])
 
-  for filepath in "$movie_path/"*; do
-    # @TODO: Find everything between the last two dots of the filename. Ex: My.Awesome.Movie.(2000).1080p-fanart.jpg -- find "1080p-fanart" and assign everything before that to a variable. This will be the new $movie_path_w_movie_root_filename. Change all code following accordingly.
-    # movie_path_w_movie_root_filename example: /home/jd/create_movie_directory_test/My Awesome Movie/My.Awesome.Movie
-    movie_path_w_movie_root_filename=$(echo $filepath | sed 's/\.\///' | cut -d "(" -f 1 | sed 's/\.$//')
-    # @TODO: Find everything between the last two dots of the filename. Ex: My.Awesome.Movie.(2000).1080p-fanart.jpg -- find "1080p-fanart" and assign everything before that to a variable. This will be the new $movie_root_filename. Change all code following accordingly.
-    # movie_root_filename example: My.Awesome.Movie
-    movie_root_filename=$(basename "$movie_path_w_movie_root_filename")
-    movie_titlename=$(echo $movie_root_filename | sed 's/\./ /g')
+  for file_w_path in "$movie_path/"*; do
+    movie_path_wo_file_ext=$(echo $file_w_path | sed 's/\.\///' | cut -d "(" -f 1 | sed 's/\.$//')
+    file_name_wo_ext=$(basename "$movie_path_wo_file_ext")
+    movie_titlename=$(echo $file_name_wo_ext | sed 's/\./ /g')
     movie_titlename_upper=$(echo $movie_titlename | tr [:lower:] [:upper:])
-    base_filename=$(basename "$filepath")
-    new_movie_path="$movie_path/$movie_root_filename"
+    base_filename=$(basename "$file_w_path")
+    new_movie_path="$movie_path/$file_name_wo_ext"
   done
 
   echo ===========================================================================
-  echo "Processing \"$movie_titlename_upper\""
-  echo "in $movie_path"
+  echo "Processing $movie_titlename_upper"
+  echo "in \"$movie_path\""
   echo ===========================================================================
   echo
-  echo "    Processing \"$base_filename\""
-  echo
 
-  for filepath in "$movie_path/"*; do
-    base_filename=$(basename "$filepath")
+  for file_w_path in "$movie_path/"*; do
+    base_filename=$(basename "$file_w_path")
     movie_files+=("$base_filename")
+    echo "    Processing \"$base_filename\"..."
   done
 
+  echo
   echo "    Copying all associated movie files back to the $movie_root..."
   echo
-  cp "$(pwd)/$folder/"* .
+  cp "$(pwd)/$directory/"* .
   echo "    Verifying files were successfully copied..."
   echo
 
-  for filepath in "$new_movie_path"*; do
-    new_base_filename=$(basename "$filepath")
+  for file_w_path in "$new_movie_path"*; do
+    new_base_filename=$(basename "$file_w_path")
     new_movie_files+=("$new_base_filename")
   done
 
@@ -85,10 +84,10 @@ find . -maxdepth 1 ! -path . -type d | sed 's/.\///' | while read -r folder; do
   if [ "${#movie_files[@]}" -eq "$match" ]; then
     echo "    Files were successfully verified!"
     echo
-    echo "    Cleaning up & deleting the movie folder..."
+    echo "    Cleaning up & deleting the movie directory..."
     echo
     echo
-    rm -r "$(pwd)/$folder/"
+    rm -r "$(pwd)/$directory/"
   else
     echo
     echo -e "    ${RED}$cleanup_msg${NC}"
@@ -96,4 +95,8 @@ find . -maxdepth 1 ! -path . -type d | sed 's/.\///' | while read -r folder; do
     echo
   fi
 done
+
+echo "Process is complete."
+echo
+echo
 
