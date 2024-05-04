de41d96c4cafe118411a2b82c59703bcbc1a71c5
J.D. Stone <jdstone1@gmail.com>
2024-05-04 16:45:00 -0700

    New script and script updates (#4)
    * rename_files-replace_spaces_with_underscores.sh
      * add documentation at top
      * change `#!/bin/bash` to `#!/usr/bin/env bash`
      * fix for the script attempting to rename itself when processing files
    * New script - easily update the dynamic IP of your hosts (subdomains) registered with Namecheap

--- rename_files-replace_spaces_with_underscores.sh
+++ rename_files-replace_spaces_with_underscores.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-for file in *; do mv "$file" `echo $file | tr ' ' '_'`; done
+#!/usr/bin/env bash
+
+#title            :rename_files-replace_spaces_with_underscores.sh
+#description      :Rename all files, replacing all spaces with underscores.
+#author           :J.D. Stone
+#usage            :Copy this script to the directory containing the files you want renamed and then run the script.
+
+
+SCRIPT_FILENAME=$(basename "$0")
+
+for file in *; do
+  if [ "$file" != "${SCRIPT_FILENAME}" ]; then
+    mv "$file" `echo $file | tr ' ' '_'`
+  fi
+done
